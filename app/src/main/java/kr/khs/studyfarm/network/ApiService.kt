999751c5406abc87c85bd9a3260b063213c16d22
KHS-kr <ks96ks@ajou.ac.kr>
2020-10-13 20:17:04 +0900

    TODO 추가 : 헤더 고정

--- app/src/main/java/kr/khs/studyfarm/network/ApiService.kt
+++ app/src/main/java/kr/khs/studyfarm/network/ApiService.kt
@@ -2,6 +2,7 @@ package kr.khs.studyfarm.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -14,22 +15,31 @@ private const val BASE_URL = "http://3.214.168.45:8080/api/v1/"
 
 enum class ApiStatus { LOADING, ERROR, DONE }
 
+//TODO - 헤더 한번에 고정시키기(지금은 못찾겟음)
+private val headerInterceptor = Interceptor {
+    val request = it.request()
+        .newBuilder()
+        .addHeader("Content-Type", "application/hal+json;charset=UTF-8")
+        .addHeader("Accept", "application/hal+json")
+        .build()
+    return@Interceptor it.proceed(request)
+}
+
 private val loggingInterceptor = HttpLoggingInterceptor().apply {
     level = HttpLoggingInterceptor.Level.BODY
 }
 private val clientBuilder = OkHttpClient.Builder().apply {
     addInterceptor(loggingInterceptor)
+//    addInterceptor(headerInterceptor)
 }
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
-
-
 interface StudyFarmApiService {
     @Headers("Content-Type: application/hal+json;charset=UTF-8",
-    "Accept: application/hal+json")
+        "Accept: application/hal+json")
     @POST("user")
     suspend fun addUser(@Body user : User) : Response
 }
