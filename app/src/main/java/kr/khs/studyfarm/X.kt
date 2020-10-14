e423fe131c30120b2f34aaf86bfd30354e8c78b8
KHS-kr <ks96ks@ajou.ac.kr>
2020-10-14 11:02:47 +0900

    이메일, 비밀번호 정규식 체크하는 함수, 나중에 viewmodel에 추가

--- app/src/main/java/kr/khs/studyfarm/X.kt
+++ app/src/main/java/kr/khs/studyfarm/X.kt
@@ -1,2 +1,9 @@
 package kr.khs.studyfarm
 
+import java.util.regex.Pattern
+
+//이메일 정규식 체크
+fun isEmailValidate(str : String) = Pattern.matches("0-9a-zA-Z@0-9a-zA-Z.[a-zA-Z]{2,3}\$", str)
+
+//비밀번호 정규식 체크
+fun isPasswordValidate(str : String) = Pattern.matches("^.(?=^.{8,15}\$)(?=.\\d)(?=.[a-zA-Z])(?=.[!@#\$%^&+=]).*\$", str)
