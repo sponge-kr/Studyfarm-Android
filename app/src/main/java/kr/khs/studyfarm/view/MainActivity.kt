f4910d0f89e08a098d7bae4b469c29c6510b4906
KHS-kr <ks96ks@ajou.ac.kr>
2020-12-28 14:11:35 +0900

    add navigation up button, no label in signup process

--- app/src/main/java/kr/khs/studyfarm/view/MainActivity.kt
+++ app/src/main/java/kr/khs/studyfarm/view/MainActivity.kt
@@ -2,11 +2,25 @@ package kr.khs.studyfarm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import kr.khs.studyfarm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
+        val navController = this.findNavController(R.id.main_navhost)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.main_navhost)
+        return navController.navigateUp()
     }
 }
