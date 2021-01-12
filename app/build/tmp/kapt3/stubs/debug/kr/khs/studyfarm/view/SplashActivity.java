68e36349bc6e94d7feb2ff8976303cd1797b8434
KHS-kr <ks96ks@ajou.ac.kr>
2021-01-12 15:43:53 +0900

    add check token

--- app/build/tmp/kapt3/stubs/debug/kr/khs/studyfarm/view/SplashActivity.java
+++ app/build/tmp/kapt3/stubs/debug/kr/khs/studyfarm/view/SplashActivity.java
@@ -2,14 +2,29 @@ package kr.khs.studyfarm.view;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\t\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0005\u001a\u00020\u0006H\u0002J\b\u0010\u0007\u001a\u00020\u0006H\u0016J\u0012\u0010\b\u001a\u00020\u00062\b\u0010\t\u001a\u0004\u0018\u00010\nH\u0014R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082D\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u000b"}, d2 = {"Lkr/khs/studyfarm/view/SplashActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "DELAY_TIME", "", "makeFullScreen", "", "onBackPressed", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\f\u001a\u00020\r2\u0006\u0010\u000e\u001a\u00020\u000fH\u0002J\b\u0010\u0010\u001a\u00020\rH\u0002J\b\u0010\u0011\u001a\u00020\rH\u0016J\u0012\u0010\u0012\u001a\u00020\r2\b\u0010\u0013\u001a\u0004\u0018\u00010\u0014H\u0014J\b\u0010\u0015\u001a\u00020\rH\u0014R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001f\u0010\u0007\u001a\u0010\u0012\f\u0012\n\u0012\u0006\b\u0001\u0012\u00020\u00010\t0\b\u00a2\u0006\b\n\u0000\u001a\u0004\b\n\u0010\u000b\u00a8\u0006\u0016"}, d2 = {"Lkr/khs/studyfarm/view/SplashActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "coroutineScope", "Lkotlinx/coroutines/CoroutineScope;", "job", "Lkotlinx/coroutines/CompletableJob;", "next", "Landroidx/lifecycle/MutableLiveData;", "Ljava/lang/Class;", "getNext", "()Landroidx/lifecycle/MutableLiveData;", "checkTokenAvailable", "", "token", "", "makeFullScreen", "onBackPressed", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "onDestroy", "app_debug"})
 public final class SplashActivity extends androidx.appcompat.app.AppCompatActivity {
-    private final long DELAY_TIME = 2000L;
+    private final kotlinx.coroutines.CompletableJob job = null;
+    private final kotlinx.coroutines.CoroutineScope coroutineScope = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.MutableLiveData<java.lang.Class<? extends androidx.appcompat.app.AppCompatActivity>> next = null;
     private java.util.HashMap _$_findViewCache;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.MutableLiveData<java.lang.Class<? extends androidx.appcompat.app.AppCompatActivity>> getNext() {
+        return null;
+    }
     
     @java.lang.Override()
     protected void onCreate(@org.jetbrains.annotations.Nullable()
     android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void checkTokenAvailable(java.lang.String token) {
+    }
+    
+    @java.lang.Override()
+    protected void onDestroy() {
     }
     
     @java.lang.Override()
