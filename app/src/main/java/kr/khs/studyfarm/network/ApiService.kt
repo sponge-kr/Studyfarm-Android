bd781242154c7ba8c86ae0d3d4a9b4f289f0e30b
KHS-kr <ks96ks@ajou.ac.kr>
2021-01-14 11:47:37 +0900

    add send code, check code network function, add data class for send code

--- app/src/main/java/kr/khs/studyfarm/network/ApiService.kt
+++ app/src/main/java/kr/khs/studyfarm/network/ApiService.kt
@@ -2,10 +2,7 @@ package kr.khs.studyfarm.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import kr.khs.studyfarm.network.request.KakaoSignupData
-import kr.khs.studyfarm.network.request.LoginData
-import kr.khs.studyfarm.network.request.User
-import kr.khs.studyfarm.network.request.UserInfo
+import kr.khs.studyfarm.network.request.*
 import kr.khs.studyfarm.network.response.CheckTokenResponse
 import kr.khs.studyfarm.network.response.GetUserResponse
 import kr.khs.studyfarm.network.response.Response
@@ -104,6 +101,15 @@ interface StudyFarmApiService {
     @GET("auth/check-token")
     suspend fun checkToken(@Header("check_token") token : String) : CheckTokenResponse
 
+    @Headers("Content-Type: application/hal+json;charset=UTF-8",
+        "Accept: application/hal+json")
+    @POST("utils/send-mail/code")
+    suspend fun sendCode(@Body email : EmailData) : Response
+
+    @Headers("Content-Type: application/hal+json;charset=UTF-8",
+        "Accept: application/hal+json")
+    @GET("auth/check-code")
+    suspend fun checkCode(@Query("email") email : String, @Query("code") code : String) : Response
 }
 
 object StudyFarmApi {
